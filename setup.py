50761d050146de93d04aa8fe37ca9bcc04d55ac0
toddkarin <toddkarin@lbl.gov>
2021-09-07 10:16:12 -0700

    Increase version

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pvarc",
-    version="0.0.9",
+    version="0.1.0",
     author="toddkarin",
     author_email="pvtools.lbl@gmail.com",
     description="Analyze anti-reflection coatings on the air-glass interface of a photovoltaic module.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DuraMAT/pvarc",
+    # package_dir={"":"pvarc"},
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=['numpy','pandas','scipy','matplotlib','tmm','colour-science','tqdm'],
