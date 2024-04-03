d9d918063dbd875f68330f779b64ef642058a80c
Todd Karin <toddakarin@gmail.com>
2024-04-03 06:02:44 -0700

    Added simulation of pv module

---
+++ examples/example_pv_module.py
@@ -0,0 +1,217 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from pvarc import thin_film_reflectance, pv_stack_absorbance
+from pvarc.materials import refractive_index_porous_silica, \
+    refractive_index_glass, refractive_index_silicon, refractive_index_eva, refractive_index_SiN
+from pvarc.color import spectrum_to_rgb
+
+
+# Wavelength axis
+# wavelength = np.arange(200, 1251, 1).astype('float')
+wavelength = np.arange(300, 1201, 1)
+
+aoi = np.arange(0,90,1)
+
+index_glass_coating = refractive_index_porous_silica(wavelength, porosity=0.28)
+index_glass = refractive_index_glass(wavelength)
+print('index glass ', index_glass)
+index_encapsulant = refractive_index_eva(wavelength)
+index_cell_coating = refractive_index_SiN(wavelength)
+index_cell = refractive_index_silicon(wavelength)
+
+absorbance = np.zeros((len(aoi), len(wavelength)))
+
+thickness_cell=260e3
+cell_arc_physical_improvement_factor=5
+thickness_cell_coating=70
+thickness_encapsulant = 0.45e-3*1e9
+thickness_glass = 2.0e-3*1e9
+thickness_glass_coating = 135
+index_air =1.0003
+
+
+index_air = 1
+index_glass_coating = 1
+thickness_glass_coating = 1
+index_glass = 1
+thickness_glass = 1e-3*1e9
+thickness_encapsulant = 1
+index_encapsulant = 1
+thickness_cell_coating = 100
+index_cell_coating = 1
+cell_arc_physical_improvement_factor = 1
+
+for k in range(len(aoi)):
+    ret = pv_stack_absorbance(
+                    index_glass_coating=index_glass_coating,
+                        index_glass=index_glass,
+                        index_encapsulant=index_encapsulant,
+                        index_cell_coating=index_cell_coating,
+                        index_cell=index_cell,
+                        thickness_glass_coating=thickness_glass_coating,
+                        thickness_glass=thickness_glass,
+                        thickness_encpsulant=thickness_encapsulant,
+                        thickness_cell_coating=thickness_cell_coating,
+                        thickness_cell=thickness_cell,
+                        wavelength=wavelength,
+                        cell_arc_physical_improvement_factor=cell_arc_physical_improvement_factor,
+                        glass_transmission_improvement_factor=2,
+                          aoi=aoi[k],
+                          polarization='mixed',
+                          index_air=1.0003
+        )
+    absorbance[k,:] = ret['EQE']
+
+
+ret = pv_stack_absorbance(
+                    index_glass_coating=index_glass_coating,
+                        index_glass=index_glass,
+                        index_encapsulant=index_encapsulant,
+                        index_cell_coating=index_cell_coating,
+                        index_cell=index_cell,
+                        thickness_glass_coating=130,
+                        thickness_glass=2.0e-3*1e9,
+                        thickness_encpsulant=0.45e-3*1e9,
+                        thickness_cell_coating=thickness_cell_coating,
+                        thickness_cell=thickness_cell,
+                          wavelength=wavelength,
+                        cell_arc_physical_improvement_factor=cell_arc_physical_improvement_factor,
+                        glass_transmission_improvement_factor=2,
+                          aoi=0,
+                          polarization='mixed',
+                          index_air=1.0003
+        )
+
+iam = absorbance/absorbance[0,:]
+color_list = [
+    '#9400D3',  # Darker Violet
+    '#4B0082',  # Indigo (kept the same as it's already a distinct deep blue)
+    '#0000FF',  # Blue
+    '#00FF00',  # Green
+    '#CCCC00',  # Darker Yellow
+    '#FF7F00',  # Orange
+    '#FF0000',  # Red
+    '#A52A2A'  # Brown
+]
+# plot each IAM function:
+plt.figure(0,figsize=(4.5,3.5))
+plt.clf()
+wavelength_to_plot = [365, 430,530,625,730,850,970,1050]
+idx_to_plot = [np.argmin(np.abs(wa - wavelength)) for wa in wavelength_to_plot]
+for k in range(len(idx_to_plot)):
+
+    plt.plot(aoi, 100*iam[:,idx_to_plot[k]], label='{} nm'.format(wavelength[idx_to_plot[k]]),
+             color=color_list[k])
+plt.xlabel('AOI (degrees)')
+plt.ylabel('IAM')
+# plt.title('Absorbance vs. AOI')
+plt.legend(loc='lower left',fontsize=8)
+plt.grid('on')
+plt.ylim([75,130])
+plt.xlim([0,90])
+plt.show()
+plt.savefig('pv_module_sim IAM.png',
+            dpi=300,
+            bbox_inches='tight')
+
+print(absorbance[:,-1])
+
+T1 = ret['Transmittance Glass ARC to Glass']
+
+plt.figure(1,figsize=(4.5,3.5))
+plt.clf()
+# fill between 0 and EQE
+plt.fill_between(wavelength,0*wavelength, ret['EQE'],label='EQE')
+
+plt.fill_between(wavelength,ret['EQE'],ret['Transmittance Glass ARC to Glass']* \
+                (1-ret['Absorbance Glass']) * \
+                 ret['Transmittance Glass to Encapsulant'] * \
+                (1-ret['Absorbance Encapsulant']) * \
+                 ret['Transmittance Through Cell ARC to Cell'],
+                 label='Rear loss'
+                 )
+plt.fill_between(wavelength,ret['Transmittance Glass ARC to Glass']* \
+                (1-ret['Absorbance Glass']) * \
+                 ret['Transmittance Glass to Encapsulant'] * \
+                 (1-ret['Absorbance Encapsulant']) * \
+                 ret['Transmittance Through Cell ARC to Cell'],
+                ret['Transmittance Glass ARC to Glass']* \
+                (1-ret['Absorbance Glass']) * \
+                 ret['Transmittance Glass to Encapsulant'] * \
+                 (1-ret['Absorbance Encapsulant']),
+                 label='Cell ARC Reflection'
+                 )
+
+plt.fill_between(wavelength,
+                ret['Transmittance Glass ARC to Glass']* \
+                (1-ret['Absorbance Glass']) * \
+                 ret['Transmittance Glass to Encapsulant'] * \
+                 (1-ret['Absorbance Encapsulant']),
+                 ret['Transmittance Glass ARC to Glass'] * \
+                 (1 - ret['Absorbance Glass']) * \
+                 ret['Transmittance Glass to Encapsulant'],
+                 label='Absorbance Encapsulant'
+                 )
+
+
+plt.fill_between(wavelength,
+                ret['Transmittance Glass ARC to Glass']* \
+                (1-ret['Absorbance Glass']) * \
+                 ret['Transmittance Glass to Encapsulant'],
+                 ret['Transmittance Glass ARC to Glass'] * \
+                 (1 - ret['Absorbance Glass']),
+                 label='Glass/Encapsulant Reflection'
+                 )
+plt.fill_between(wavelength,
+                 ret['Transmittance Glass ARC to Glass'] * \
+                 (1 - ret['Absorbance Glass']),
+                ret['Transmittance Glass ARC to Glass'],
+                 label='Glass Absorbance'
+                 )
+plt.fill_between(wavelength,
+                ret['Transmittance Glass ARC to Glass'],
+                 1*wavelength,
+                 label='Glass ARC'
+                 )
+
+
+# plt.plot(wavelength, ret['EQE'], label='EQE')
+# plt.plot()
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('Absorbance')
+# plt.title('Absorbance vs. Wavelength')
+plt.legend(loc=[0.2,0.1],fontsize=8)
+plt.grid('on')
+plt.xlim([300,1200])
+plt.ylim([0,1])
+plt.show()
+plt.savefig('pv_module_sim Loss Analysis.png',
+            dpi=300,
+            bbox_inches='tight')
+
+plt.figure(2)
+plt.clf()
+
+for key in ret:
+    print(key)
+    if len(ret[key])==1:
+        ret[key] = ret[key] * np.ones(len(wavelength))
+
+    plt.plot(wavelength, ret[key], label=key)
+# plt.plot(wavelength, ret['Transmittance Through Cell ARC to Cell'],label='Cell ARC')
+# plt.plot(wavelength, ret['Transmittance Glass ARC to Glass'],label='Glass ARC')
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('Transmittance Through Cell ARC to Cell')
+plt.legend()
+plt.grid('on')
+plt.ylim([0,1])
+plt.show()
+
+plt.savefig('pv_module_sim Components.png',
+            dpi=300,
+            bbox_inches='tight')
--- pvarc/main.py
+++ pvarc/main.py
@@ -68,6 +68,7 @@ def thin_film_reflectance(index_film,
 
     # Calculate angles from Snell's law
     theta0 = aoi * pi / 180
+    print('TODO: need to add checks that the calculated value is not off by pi')
     theta1 = np.arcsin(index_air / index_film * sin(theta0))
     theta2 = np.arcsin(index_air / index_substrate * sin(theta0))
 
@@ -113,9 +114,23 @@ def thin_film_reflectance(index_film,
         raise Exception('Polarization must be "mixed", "s" or "p".')
 
 
+def thin_film_transmittance(index_film,
+                          index_substrate,
+                          film_thickness,
+                          wavelength,
+                          aoi=8.0,
+                          polarization='mixed',
+                          index_air=1.0003):
+    return 1 - thin_film_reflectance(index_film,
+                            index_substrate,
+                            film_thickness,
+                            wavelength,
+                            aoi=aoi,
+                            polarization=polarization,
+                            index_air=index_air)
 
 
-def single_interface_reflectance(n0,n1, aoi=8.0, polarization='mixed'):
+def single_interface_reflectance(n0, n1, aoi=8.0, polarization='mixed'):
     """
     Reflection from a single interface for a wave traveling from material n0
     to n1. Both n0 and n1 can be an array of values.
@@ -157,4 +172,190 @@ def single_interface_reflectance(n0,n1, aoi=8.0, polarization='mixed'):
     else:
         raise Exception('Polarization must be "mixed", "s" or "p".')
 
+
+def single_interface_transmittance(n0, n1, aoi=8.0, polarization='mixed'):
+    """
+    Transmittance from a single interface for a wave traveling from material n0
+    to n1. Both n0 and n1 can be an array of values.
+
+    Parameters
+    ----------
+    n0 : ndarray or float
+        Index of refraction for semi-infinite material with incident wave.
+
+    n1 : ndarray or float
+        Index of refraction for semi-infinite material that wave reflects from.
+
+    aoi : ndarray or float
+        Angle of incidence in degrees.
+
+    polarization : str
+        Polarization of wave, can be 's', 'p' or 'mixed'.
+
+    Returns
+    -------
+    reflectance : ndarray
+        Reflectance of interface.
+
+    """
+
+    theta0 = aoi * pi / 180
+    theta1 = np.arcsin(n0 / n1 * sin(theta0))
+
+    ts01 = 2 * n0 * cos(theta0) / (n0 * cos(theta0) + n1 * cos(theta1))
+    tp01 = 2 * n0 * cos(theta0) / (n1 * cos(theta0) + n0 * cos(theta1))
+
+    T_s = np.abs(ts01) ** 2 * np.real(n1 * cos(theta1)) / np.real(n0 * cos(theta0))
+    T_p = np.abs(tp01) ** 2 * np.real(n1 * cos(np.conjugate(theta1))) / np.real(n0 * cos(np.conjugate(theta0)))
+
+    if polarization == 'mixed':
+        return 0.5 * (T_s + T_p)
+    elif polarization == 's':
+        return T_s
+    elif polarization == 'p':
+        return T_p
+    else:
+        raise Exception('Polarization must be "mixed", "s" or "p"')
+
+def thick_film_absorbance(n,thickness,wavelength,aoi):
+    alpha = 4 * pi * np.imag(n) / wavelength
+    eff_thickness = np.real(thickness / cos(aoi/180*pi))
+    delta = alpha * eff_thickness
+
+    # intensity reduction (i.e. absorbance) in the cell
+
+    return 1 - np.exp(-delta)
+
+def pv_stack_absorbance(index_glass_coating,
+                        index_glass,
+                        index_encapsulant,
+                        index_cell_coating,
+                        index_cell,
+                        thickness_glass_coating,
+                        thickness_glass,
+                        thickness_encpsulant,
+                        thickness_cell_coating,
+                        thickness_cell,
+                        wavelength,
+                        cell_arc_physical_improvement_factor=2,
+                        glass_transmission_improvement_factor=2,
+                          aoi=8.0,
+                          polarization='mixed',
+                          index_air=1.0003):
+    """
+    Calculate absorbance in a PV cell using a simplified one-way model.
+
+    The method is derived from the matrix form of the Fresnel equations,
+    detailed in Ref. [1].
+
+    [1] https://arxiv.org/abs/1603.02720
+
+    Parameters
+    ----------
+
+    index_film : ndarray or float
+
+        Refractive index of the thin film at the values specified in the
+        `wavelength` input.
+
+    index_substrate : ndarray or float
+
+        Refractive index of the substrate at the values specified in the
+        `wavelength` input.
+
+    film_thickness : ndarray or float
+
+        Thickness of thin film in nm.
+
+    wavelength : ndarray or float
+
+        Wavelength in nm
+
+    aoi : ndarray or float
+
+        Angle of incidence of light onto thin film, in degrees.
+
+    polarization : str
+
+        Polarization of light, can be 's', 'p' or 'mixed'.
+
+    index_air : ndarray or float
+
+        Index of refraction of air, default is 1.0003
+
+    Returns
+    -------
+    reflectance : ndarray or float
+
+        Fractional reflectance for each value in the arrays of input
+        parameters. Will have same size as input arrays.
+
+    """
+    # Layer numbering:
+    # 0: air
+    # 1: glass coating
+    # 2: glass
+    # 3: encapsulant
+    # 4: cell coating
+    # 5: Cell
+
+    # Calculate angles from Snell's law
+    theta0 = aoi * pi / 180
+    theta1 = np.arcsin(index_air / index_glass_coating * sin(theta0))
+    theta2 = np.arcsin(index_air / index_glass * sin(theta0))
+    theta3 = np.arcsin(index_air / index_encapsulant * sin(theta0))
+    theta4 = np.arcsin(index_air / index_cell_coating * sin(theta0))
+    theta5 = np.arcsin(index_air / index_cell * sin(theta0))
+
+
+    # Give index of refraction values better names.
+    n0 = index_air
+    n1 = index_glass_coating
+    n2 = index_glass
+    n3 = index_encapsulant
+    n4 = index_cell_coating
+    n5 = index_cell
+
+    # Calculate transmittance through the first stack (the Glass Coating)
+    T1 = thin_film_transmittance(index_film=n1,
+                                 index_substrate=n2,
+                                 film_thickness=thickness_glass_coating,
+                                 wavelength=wavelength,
+                                 aoi=aoi,
+                                 polarization=polarization,
+                                 index_air=n0)
+
+
+    # Absorbance in the glass:
+    A2 = thick_film_absorbance(n=n2,thickness=thickness_glass,wavelength=wavelength,aoi=theta2*180/pi)
+    A2 = A2 / glass_transmission_improvement_factor
+
+    # Transmittance from glass to encapsulant
+    T2 = single_interface_transmittance(n0=n2, n1=n3, aoi=theta2*180/pi, polarization=polarization)
+
+    # Absorbance in the encapsulant
+    A3 = thick_film_absorbance(n=n3, thickness=thickness_encpsulant, wavelength=wavelength, aoi=theta3 * 180 / pi)
+
+    # Transmittance through cell ARC into the cell
+    T4 = thin_film_transmittance(index_film=n4,
+                                 index_substrate=n5,
+                                 film_thickness=thickness_cell_coating,
+                                 wavelength=wavelength,
+                                 aoi=theta3/pi*180,
+                                 polarization=polarization,
+                                 index_air=n3)
+    T4 = 1 - (1 - T4) / cell_arc_physical_improvement_factor
+    A5 = thick_film_absorbance(n=n5,thickness=thickness_cell,wavelength=wavelength,aoi=theta5*180/pi)
+
+    # put it all together to get the fraction of incoming light absorbed by the cell
+    ret = {'EQE': np.real(T1 * (1-A2) * T2 *  (1-A3) * T4 * A5),
+           'Transmittance Glass ARC to Glass': np.atleast_1d(T1),
+           'Absorbance Glass': np.atleast_1d(A2),
+           'Transmittance Glass to Encapsulant': np.atleast_1d(T2),
+           'Absorbance Encapsulant': np.atleast_1d(A3),
+           'Transmittance Through Cell ARC to Cell': np.atleast_1d(T4),
+           'Absorbance Cell': np.atleast_1d(A5),
+           }
+
+    return ret
 
